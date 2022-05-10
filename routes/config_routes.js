7e1485db849f6c2212d34bde37984c5092771308
Ariel-levi <38177520+Ariel-levi@users.noreply.github.com>
2022-05-10 14:28:02 +0300

    v

--- routes/config_routes.js
+++ routes/config_routes.js
@@ -10,21 +10,24 @@ const uploadFilesR = require("./uploadFiles");
 
 // מאפשר לשרת בדומיין אחר לבצע בקשות לשרת שלנו דרך דפדפן
 exports.corsAccessControl = (app) => {
-  app.all('*', function (req, res, next) {
-    if (!req.get('Origin')) return next();
+  app.all("*", function (req, res, next) {
+    if (!req.get("Origin")) return next();
     // * -> במציאות במקום כוכבית נכניס שם דומיין שיש לו אישור גישה
     // לשרת
-    res.set('Access-Control-Allow-Origin', '*');
+    res.set("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
-    res.set('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,auth-token');
+    res.set(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,Content-Type,auth-token"
+    );
     next();
   });
-}
+};
 // פונקציה שנקרא לה באפ ומגדירה לפי הכתובת שהיוזר
 // הגיע איזה ראוט להפעיל במידה והקובץ לא נמצא
 // בתקיית פאבליק
 exports.routesInit = (app) => {
-  app.use("/",indexR);
+  app.use("/", indexR);
   app.use("/users", usersR);
   app.use("/categories", categoriesR);
   app.use("/products", productsR);
@@ -35,9 +38,9 @@ exports.routesInit = (app) => {
   app.use("/uploadFiles", uploadFilesR);
 
   // במידה ולא הגיע לעמוד נכון , נציג לו 404
-  app.use((req,res) => {
+  app.use((req, res) => {
     //.status(404) -> מה הסטטוס של הדף
     // קריטי כדי שהצד לקוח יזהה ישר שיש לו טעות בבקשה
-    res.status(404).json({msg_error:"Url not found , 404!"})
-  })
-}
+    res.status(404).json({ msg_error: "Url not found , 404!" });
+  });
+};
