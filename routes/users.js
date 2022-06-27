3aa8aa78ea8d5e287c6272dcabdca7d1f732845c
Nati Ronen <91008729+NatiRonen@users.noreply.github.com>
2022-06-27 19:45:23 +0300

    ok

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,7 @@ const bcrypt = require("bcrypt");
 const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 const { auth, authSystemAdmin } = require("../middlewares/auth");
+require("dotenv").config();
 const { pick } = require("lodash");
 const { verifyUserEmail } = require("../middlewares/sendEmail");
 const open = require("open");
@@ -83,7 +84,7 @@ router.get("/verify-email", async (req, res) => {
         //update verified to true and remove email token
         { verified: true, $unset: { emailToken: "" } }
       );
-      open(process.env.CLIENT_URL + "/welcome");
+      open(`${process.env.CLIENT_URL} /welcome`);
       return res
         .status(200)
         .json({ msg: "You have verified your account, you can login now" });
